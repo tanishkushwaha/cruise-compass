dc69107c6b458e162a795a5b79e20483477ec20b
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-05-06 19:37:53 +0530

    Added react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
-
+import { Routes, Route } from 'react-router-dom'
+import Signin from './pages/Signin'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<Signin />} />
+      </Routes>
+
+    </>
   )
 }
 
