0cb2fd3ee31e1a155ad1b1da51b1f87f6efca590
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-05-08 17:34:26 +0530

    Add manage catering, stationery, movies pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import Signin from './pages/Signin'
 import Home from './pages/Home'
 import VoyagerAdd from './pages/VoyagerAdd'
 import VoyagerDetails from './pages/VoyagerDetails'
+import ManageCatering from './pages/ManageCatering'
+import ManageStationery from './pages/ManageStationery'
+import ManageMovies from './pages/ManageMovies'
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
         <Route path='/signin' element={<Signin />} />
         <Route path='/voyagers/add' element={<VoyagerAdd />} />
         <Route path='/voyagers/details' element={<VoyagerDetails />} />
+        <Route path='/manage/catering' element={<ManageCatering />} />
+        <Route path='/manage/stationery' element={<ManageStationery />} />
+        <Route path='/manage/movies' element={<ManageMovies />} />
       </Routes>
 
     </>
