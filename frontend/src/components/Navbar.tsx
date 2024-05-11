db5843a335a49b8439f110e73e873588b5aa13b0
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-05-11 20:44:38 +0530

    Fix scrolling issue

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,26 +3,25 @@ import { Outlet } from "react-router-dom"
 import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
-
   return (
     <>
-      <Grid h='100vh' templateColumns='repeat(12, 1fr)' >
+      <Grid templateColumns='repeat(12, 1fr)' >
         <GridItem colSpan={2} bg='black' p={5}>
-          <Flex justifyContent='center' alignItems='center' flexDirection='column' gap={3} mb='5rem'>
+          <Flex justifyContent='center' alignItems='center' flexDirection='column' gap={3} mb='3rem'>
             <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' size='lg' />
-            <Text fontSize='xl' letterSpacing={1} color='white' as='b'>Dan Abrahmov</Text>
+            <Text fontSize='lg' letterSpacing={1} color='white' as='b'>Some Guy</Text>
           </Flex>
 
           <Flex direction='column' >
-            <NavItem title='Food' path='/order/catering/food' />
-            <NavItem title='Snacks' path='/order/catering/snacks' />
+            <NavItem title='Catering' path='/order/catering' />
+            <NavItem title='Stationery' path='/order/stationery' />
             <NavItem title='Movies' path='/book/movies' />
             <NavItem title='Beauty Salon' path='/book/beauty-salon' />
             <NavItem title='Fitness Centre' path='/book/fitness-centre' />
             <NavItem title='Party Hall' path='/book/party-hall' />
           </Flex>
         </GridItem>
-        <GridItem colSpan={10}>
+        <GridItem colSpan={10} overflow='auto' h='100vh'>
           <Outlet />
         </GridItem>
       </Grid>
@@ -35,10 +34,9 @@ const NavItem = ({ title, path }: { title: string, path: string }) => {
 
     <NavLink to={path} className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : ""}>
 
-      {({ isActive, isPending, isTransitioning }) => (
-        // <span className={isActive ? "active" : ""}>Tasks</span>
+      {({ isActive }) => (
         <Flex h='30px' px='1rem' py='2rem' alignItems='center' _hover={{ bgColor: 'gray.900' }} cursor='pointer' bgColor={isActive ? 'gray.900' : ''}>
-          <Text fontSize='lg' letterSpacing='1px' color='white' as='b'>{title}</Text>
+          <Text fontSize='md' letterSpacing='1px' color='white' as='b'>{title}</Text>
         </Flex>
       )}
 
