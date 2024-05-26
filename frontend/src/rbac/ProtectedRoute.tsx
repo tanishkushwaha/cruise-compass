9291442947eeb9e315ee911cef3b042bda787a95
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-05-26 17:02:32 +0530

    Refactor protected route logic

---
+++ frontend/src/rbac/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom"
+import { useAuth } from "../context/authContext"
+import Forbidden from "../pages/others/Forbidden"
+import SpinnerScreen from "../components/SpinnerScreen"
+
+const ProtectedRoute = ({ roles, children }: any) => {
+  const auth = useAuth()
+
+  if (auth.loading) return <SpinnerScreen />
+  else {
+    if (!auth.user.loggedIn) return <Navigate to='/login' />
+    else {
+      if (roles.includes(auth.user.role)) {
+        return children
+      } else {
+        return <Forbidden />
+      }
+    }
+
+  }
+}
+export default ProtectedRoute
