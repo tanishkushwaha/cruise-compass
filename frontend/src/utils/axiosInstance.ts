86989c66ed7f10f5b3b68d48a1fd5a5eb3151edf
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2025-09-05 16:12:06 +0530

    refactor(frontend): move backend url to env var

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+if (!import.meta.env.VITE_BACKEND_URL) {
+  throw new Error(
+    "Please set the VITE_BACKEND_URL variable in your .env file. "
+  );
+}
+
 const axiosInstance = axios.create({
   withCredentials: true, // For sending cookies with every req
-  baseURL: 'https://probable-space-computing-machine-j74j6qvq4jp2jj5p-8080.app.github.dev',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 export default axiosInstance;
